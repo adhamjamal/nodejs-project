52f7f65aa8273f664c5992d3fc39317625e3ebb3
adhamjamal <82598590+adhamjamal@users.noreply.github.com>
2022-06-29 12:13:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// mongoose configuration
+require('./config/mongoose');
+
+// dotenv configuration
+dotenv.config();
+
+// morgan configuration
+if (process.env.ENVIRONMENT !== "production") {
+  app.use(morgan("dev"));
+}
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Credential"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  return next();
+});
+
+// Server Routes
+app.use("/api/server/", require("./api/routes/server"));
+
+// Authentication Routes
+app.use("/api/authentication/", require("./api/routes/authentication"));
+
+module.exports = app;
